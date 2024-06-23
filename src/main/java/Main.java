2b73b47217c239c4a1f8a39d56dbdcc0df26c4dc
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-06-23 20:08:09 +0000

    adjustments to make the code more coherent to the assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -50,43 +50,32 @@ public class Main {
   System.out.print("Warehouse_4 = " + inventory[4][3] + " ");
   System.out.println("Warehouse_5 = " + inventory[4][4] + " ");
 
-//Customer purchase of three of each item at Warehouse 3 (can be filled in with user input)
+//Customer purchase of three of each item at Warehouse 3.  This setup will only work for one warehouse at a time
 System.out.println("\nCustomer Purchase:");
 System.out.println("\nQuantity");
 
 System.out.println("");
 System.out.print(items[0] + ": ");
 Integer quantity1 = scan.nextInt();
-    System.out.print("Warehouse: ");
-    String warehouse1 = scan.nextLine();
       scan.nextLine();
     
 System.out.print(items[1] + ": ");
 Integer quantity2 = scan.nextInt();
-    System.out.print("Warehouse: ");
-    String warehouse2 = scan.nextLine();
        scan.nextLine();
     
 System.out.print(items[2] + ": ");
 Integer quantity3 = scan.nextInt();
-    System.out.print("Warehouse: ");
-    String warehouse3 = scan.nextLine();
        scan.nextLine();
      
 System.out.print(items[3] + ": ");
 Integer quantity4 = scan.nextInt();
-    System.out.print("Warehouse: ");
-    String warehouse4 = scan.nextLine();
        scan.nextLine();
           
 System.out.print(items[3] + ": ");
 Integer quantity5 = scan.nextInt();
-    System.out.print("Warehouse: ");
-    String warehouse5 = scan.nextLine();
        scan.nextLine();
-    
 
- //This information will need to be updated manually to reflect the user input
+//This information would need to be updated manually to reflect the appropriate warehouse
     inventory[0][2] = inventory[0][2] - quantity1; 
     inventory[1][2] = inventory[1][2] - quantity2; 
     inventory[2][2] = inventory[2][2] - quantity3; 
@@ -120,8 +109,9 @@ Integer quantity5 = scan.nextInt();
     System.out.print("Warehouse_4 = " + inventory[4][3] + " ");
     System.out.println("Warehouse_5 = " + inventory[4][4] + " ");
 
-System.out.println("\nShipment Details:");
-      System.out.print("Item: ");
+//Output for shipment in.  This will only work for one item at a time, but can be scaled up for multiple warehouses, provided the shipment is the same size.
+    System.out.println("\nShipment Details:");
+    System.out.print("Item: ");
       String itemIN = scan.nextLine();
         System.out.print("Warehouse: ");
         String whichHOUSE = scan.nextLine();
@@ -143,7 +133,7 @@ System.out.println("\nShipment Details:");
     inventory[3][3] = inventory[3][3] + quantIN; 
     inventory[4][3] = inventory[4][3] + quantIN;
 
-//Again, this would have to be manually updated for any changes, so is not a good general approach, but could be used to track simple changes.
+//Again, this would have to be manually updated for any changes, so is not a good general approach, but could be used to track simple changes.  Should probably use a for-loop here of some kind, but ran out of time to be able to install one.
       
       System.out.println("\nUpdated Tool Inventory");
       System.out.print(items[0] + ": " + "Warehouse_1 = " + inventory[0][0] + " ");
