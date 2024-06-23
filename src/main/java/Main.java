164b891f408f6ccb830ad093a26d7753f418ff7b
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-06-23 17:02:50 +0000

    declare a multi-dimensional array to hold inventory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,21 @@ public class Main {
   // declare one-dimensional array holding items
   String[] items;
   // initialize array
-  items = new Strin[5];
+  items = new String[5];
       items[0] = "screwdriver"
       items[1] = "hammer"
       items[2] = "shovel"
       items[3] = "nail"
       items[4] = "screw"
+
+  // declare multi-dimensional array to hold inventory; will be integer because number of items cannot be a decimal value
+  int[][] inventory = {
+        {20, 17, 18, 12, 9}
+        {16, 23, 11, 8, 19}
+        {12, 11, 15, 13, 12}
+        {18, 22, 18, 23, 14}
+        {10, 9, 12, 16, 18}
+  }
   }
 
   
