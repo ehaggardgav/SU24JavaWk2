51e6dd4cd41da361eb3b47c2d4b017c31cc38a43
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-06-23 19:45:43 +0000

    add code to reflect shipment changes and print an updated inventory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -50,61 +50,50 @@ public class Main {
   System.out.print("Warehouse_4 = " + inventory[4][3] + " ");
   System.out.println("Warehouse_5 = " + inventory[4][4] + " ");
 
-  //Customer purchase of three of each item at Warehouse 3
+//Customer purchase of three of each item at Warehouse 3 (can be filled in with user input)
 System.out.println("\nCustomer Purchase:");
-System.out.println("\nItems");
-      
+System.out.println("\nQuantity");
+
+System.out.println("");
 System.out.print(items[0] + ": ");
-String item1 = scan.nextLine();
+Integer quantity1 = scan.nextInt();
     System.out.print("Warehouse: ");
     String warehouse1 = scan.nextLine();
-    System.out.print("Quantity: ");
-    Integer quantity1 = scan.nextInt();
-      
+      scan.nextLine();
+    
 System.out.print(items[1] + ": ");
-String item2 = scan.nextLine();
+Integer quantity2 = scan.nextInt();
     System.out.print("Warehouse: ");
     String warehouse2 = scan.nextLine();
-    System.out.print("Quantity: ");
-    Integer quantity2 = scan.nextInt();
-      
+       scan.nextLine();
+    
 System.out.print(items[2] + ": ");
-String item3 = scan.nextLine();
+Integer quantity3 = scan.nextInt();
     System.out.print("Warehouse: ");
     String warehouse3 = scan.nextLine();
-    System.out.print("Quantity: ");
-    Integer quantity3 = scan.nextInt();
-
+       scan.nextLine();
+     
 System.out.print(items[3] + ": ");
-String item4 = scan.nextLine();
+Integer quantity4 = scan.nextInt();
     System.out.print("Warehouse: ");
     String warehouse4 = scan.nextLine();
-    System.out.print("Quantity: ");
-    Integer quantity4 = scan.nextInt();
-
+       scan.nextLine();
+          
 System.out.print(items[3] + ": ");
-String item4 = scan.nextLine();
+Integer quantity5 = scan.nextInt();
     System.out.print("Warehouse: ");
-    String warehouse4 = scan.nextLine();
-    System.out.print("Quantity: ");
-    Integer quantity4 = scan.nextInt();
-
-
-        /*
-        System.out.println("Item 1:");
-        String item1 = scan.nextLine(); 
-        System.out.println("Price:");
-        float price1 = scan.nextFloat();
-        scan.nextLine();
-        */
+    String warehouse5 = scan.nextLine();
+       scan.nextLine();
     
-    inventory[0][2] = inventory[0][2] - quantity1; 
-    inventory[1][2] = inventory[1][2] - 3; 
-    inventory[2][2] = 12; 
-    inventory[3][2] = 1797; 
-    inventory[4][2] = 1197;
 
-    System.out.println("\nTool Inventory");
+ //This information will need to be updated manually to reflect the user input
+    inventory[0][2] = inventory[0][2] - quantity1; 
+    inventory[1][2] = inventory[1][2] - quantity2; 
+    inventory[2][2] = inventory[2][2] - quantity3; 
+    inventory[3][2] = inventory[3][2] - quantity4; 
+    inventory[4][2] = inventory[4][2] - quantity5;
+
+    System.out.println("\nUpdated Tool Inventory");
     System.out.print(items[0] + ": " + "Warehouse_1 = " + inventory[0][0] + " ");
     System.out.print("Warehouse_2 = " + inventory[0][1] + " ");
     System.out.print("Warehouse_3 = " + inventory[0][2] + " ");
@@ -130,6 +119,56 @@ String item4 = scan.nextLine();
     System.out.print("Warehouse_3 = " + inventory[4][2] + " ");
     System.out.print("Warehouse_4 = " + inventory[4][3] + " ");
     System.out.println("Warehouse_5 = " + inventory[4][4] + " ");
+
+System.out.println("\nShipment Details:");
+      System.out.print("Item: ");
+      String itemIN = scan.nextLine();
+        System.out.print("Warehouse: ");
+        String whichHOUSE = scan.nextLine();
+        System.out.print("Quantity: ");
+        Integer quantIN = scan.nextInt();
+            scan.nextLine();
+      
+//Warehouse 2
+    inventory[0][1] = inventory[0][1] + quantIN; 
+    inventory[1][1] = inventory[1][1] + quantIN; 
+    inventory[2][1] = inventory[2][1] + quantIN; 
+    inventory[3][1] = inventory[3][1] + quantIN; 
+    inventory[4][1] = inventory[4][1] + quantIN;
+
+//Warehouse 4
+    inventory[0][3] = inventory[0][3] + quantIN; 
+    inventory[1][3] = inventory[1][3] + quantIN; 
+    inventory[2][3] = inventory[2][3] + quantIN; 
+    inventory[3][3] = inventory[3][3] + quantIN; 
+    inventory[4][3] = inventory[4][3] + quantIN;
+
+      System.out.println("\nUpdated Tool Inventory");
+      System.out.print(items[0] + ": " + "Warehouse_1 = " + inventory[0][0] + " ");
+      System.out.print("Warehouse_2 = " + inventory[0][1] + " ");
+      System.out.print("Warehouse_3 = " + inventory[0][2] + " ");
+      System.out.print("Warehouse_4 = " + inventory[0][3] + " ");
+      System.out.println("Warehouse_5 = " + inventory[0][4] + " ");
+      System.out.print(items[1] + ": " + "Warehouse_1 = " + inventory[1][0] + " ");
+      System.out.print("Warehouse_2 = " + inventory[1][1] + " ");
+      System.out.print("Warehouse_3 = " + inventory[1][2] + " ");
+      System.out.print("Warehouse_4 = " + inventory[1][3] + " ");
+      System.out.println("Warehouse_5 = " + inventory[1][4] + " ");
+      System.out.print(items[2] + ": " + "Warehouse_1 = " + inventory[2][0] + " ");
+      System.out.print("Warehouse_2 = " + inventory[2][1] + " ");
+      System.out.print("Warehouse_3 = " + inventory[2][2] + " ");
+      System.out.print("Warehouse_4 = " + inventory[2][3] + " ");
+      System.out.println("Warehouse_5 = " + inventory[2][4] + " ");
+      System.out.print(items[3] + ": " + "Warehouse_1 = " + inventory[3][0] + " ");
+      System.out.print("Warehouse_2 = " + inventory[3][1] + " ");
+      System.out.print("Warehouse_3 = " + inventory[3][2] + " ");
+      System.out.print("Warehouse_4 = " + inventory[3][3] + " ");
+      System.out.println("Warehouse_5 = " + inventory[3][4] + " ");
+      System.out.print(items[4] + ": " + "Warehouse_1 = " + inventory[4][0] + " ");
+      System.out.print("Warehouse_2 = " + inventory[4][1] + " ");
+      System.out.print("Warehouse_3 = " + inventory[4][2] + " ");
+      System.out.print("Warehouse_4 = " + inventory[4][3] + " ");
+      System.out.println("Warehouse_5 = " + inventory[4][4] + " ");
   }
 
   
