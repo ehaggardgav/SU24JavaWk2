baee0fb02e2df3a30e165e05244b8c2a97f9cabf
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-06-23 16:46:53 +0000

    populate one-dimensional array with items

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,13 @@ public class Main {
   public static void main(String[] args) {
   // declare one-dimensional array holding items
   String[] items;
+  // initialize array
+  items = new Strin[5];
+      items[0] = "screwdriver"
+      items[1] = "hammer"
+      items[2] = "shovel"
+      items[3] = "nail"
+      items[4] = "screw"
   }
 
   
